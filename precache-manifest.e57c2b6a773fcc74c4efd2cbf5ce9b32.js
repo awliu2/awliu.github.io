1933f30aa4e47f89b9e6500475f9d95849a6f1ef
Andi Liu <awliu2@uchicago.edu>
2022-07-29 18:30:27 -0500

    Updates

---
+++ precache-manifest.e57c2b6a773fcc74c4efd2cbf5ce9b32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d15ab0a3166960f740be",
+    "url": "/static/css/main.78ce6166.chunk.css"
+  },
+  {
+    "revision": "d15ab0a3166960f740be",
+    "url": "/static/js/main.d15ab0a3.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "37d6c4bb6a1d513552a3",
+    "url": "/static/js/2.37d6c4bb.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6f250488986b4adf8ba0796818239910",
+    "url": "/index.html"
+  }
+];
